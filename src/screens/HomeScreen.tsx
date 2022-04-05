dbe51fb5ae148395b9554a0ad220b75f51dcb2c4
alexelizaga <alexelizaga@icloud.com>
2022-04-05 19:21:21 +0200

    191

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,70 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+import { globalStyles } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+interface MenuItem {
+  name: string;
+  icon: any;
+  component: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen'
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen'
+  },
+];
+
 export const HomeScreen = () => {
+
+  const {top} = useSafeAreaInsets();
+  const renderMenuItem = ( menuItem: MenuItem) => {
+    return (
+      <View style={ styles.menuItemContainer}>
+        <Ionicons name={menuItem.icon} size={16} color="green" />
+        <Text> {menuItem.name}</Text>
+      </View>
+    )
+  }
+
+  const renderListHeader = () => {
+    return (
+      <View style={{ marginTop: top + 20, marginBottom: 20 }}>
+        <Text style={globalStyles.title}>Opciones de menú</Text>
+      </View>
+    )
+  }
+
+  const itemSeparator = () => {
+    return(
+      <View
+        style={{
+          borderBottomWidth: 1,
+          opacity: 0.4,
+          marginVertical: 8
+        }}
+      />
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />
-      <Text>HomeScreen</Text>
+    <View style={[ styles.container, globalStyles.margin ]}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={ (item) => item.name }
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={itemSeparator}
+      />
     </View>
   )
 }
@@ -14,8 +72,9 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  menuItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
 });
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  margin: {
+    marginHorizontal: 20
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold'
+  }
+});
