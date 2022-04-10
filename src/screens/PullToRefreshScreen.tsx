b5cc5a3382f056257026c295b3dd473925e3c8eb
alexelizaga <alexelizaga@icloud.com>
2022-04-10 11:21:28 +0200

    210

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, RefreshControl } from 'react-native';
+import { View, Text, ScrollView, RefreshControl, useWindowDimensions, Platform } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
-import { globalStyles } from '../theme/appTheme';
+import { globalStyles, colors } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const PullToRefreshScreeen = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>()
+  const { height } = useWindowDimensions();
+  const { top } = useSafeAreaInsets();
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -12,21 +16,31 @@ export const PullToRefreshScreeen = () => {
     setTimeout(() => {
       console.log('Terminamos');
       setRefreshing(false);
-    }, 1500);
+      setData('Hola mundo');
+    }, 15000);
   }
 
   return (
-    <ScrollView
+    <ScrollView style={{ marginTop: (refreshing && Platform.OS === 'ios') ? top : 0}}
       refreshControl={
         <RefreshControl
           refreshing={refreshing}
           onRefresh={onRefresh}
+          progressViewOffset={0.4*height}
+          // progressBackgroundColor={colors.primary}
+          colors={[ colors.primaryLight, colors.primary ]}
+          // style={{ backgroundColor:colors.primary }}
+          // tintColor={'white'}
+          title={'Refreshing'}
         />
       }
     >
       <View style={globalStyles.margin}>
         <HeaderTitle title='Pull to refresh' />
         <Text>PullToRefreshScreeen</Text>
+        {
+          data && <Text>{data}</Text>
+        }
       </View>
     </ScrollView>
   )
