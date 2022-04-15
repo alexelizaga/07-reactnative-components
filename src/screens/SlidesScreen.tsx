46d915e786b4a1fce6cebba90ae0e9aeabd4b9e3
alexelizaga <alexelizaga@icloud.com>
2022-04-15 12:22:54 +0200

    225

--- src/screens/SlidesScreen.tsx
+++ src/screens/SlidesScreen.tsx
@@ -1,12 +1,80 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { HeaderTitle } from '../components/HeaderTitle';
+import { View, Text, ImageSourcePropType, Dimensions, Image, StyleSheet } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors } from '../theme/appTheme';
+
+const { height: screenheight, width: screenWidth } = Dimensions.get('window');
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType
+}
+
+const items: Slide[] = [
+  {
+      title: 'Titulo 1',
+      desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+      img: require('../assets/slide-1.png')
+  },
+  {
+      title: 'Titulo 2',
+      desc: 'Anim est quis elit proident magna quis cupidatat culpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+      img: require('../assets/slide-2.png')
+  },
+  {
+      title: 'Titulo 3',
+      desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+      img: require('../assets/slide-3.png')
+  },
+]
 
 export const SlidesScreen = () => {
+  const renderItem = (item: Slide) => {
+    return (
+      <View style={{
+        flex:1,
+        backgroundColor:'white',
+        borderRadius: 5,
+        padding: 40,
+        justifyContent: 'center'
+      }}>
+        <Image
+          source={ item.img }
+          style={{
+            width: 350,
+            height: 400,
+            resizeMode: 'center'
+          }}
+        />
+        <Text style={styles.title}>{ item.title }</Text>
+        <Text style={styles.subTitle}>{ item.desc }</Text>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <HeaderTitle title='Slides' />
-      <Text>SlidesScreen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <Carousel
+        // ref={(c) => { this._carousel = c; }}
+        data={items}
+        renderItem={ ({item}) => renderItem(item) }
+        sliderWidth={screenWidth}
+        itemWidth={screenWidth}
+        layout={'default'}
+      />
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+    title:{
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: colors.primary
+    },
+    subTitle: {
+      fontSize: 16
+    }
+});
